e74e5e6479ac72d0db1a158ceb0f03f65d0d0bb3
Hao Tang <haotang.io@gmail.com>
2019-12-23 15:11:41 +0700

    refactor: remove @zeit/next-typescript as it is not required anymore

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-const withTypescript = require('@zeit/next-typescript');
 const withSass = require('@zeit/next-sass');
 
-const config = withTypescript(withSass());
+const config = withSass();
 
 config.publicRuntimeConfig = {
   BASE_URL: process.env.BASE_URL,
