834ccfa048faa260203f96df30678d4a98d677bd
Hao Tang <haotang.io@gmail.com>
2020-01-08 17:36:19 +0700

    refactor: guestOnly HOC

--- hocs/guestOnly/index.tsx
+++ hocs/guestOnly/index.tsx
@@ -2,8 +2,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
-import { NextComponentType, NextContext } from 'next';
-import { SingletonRouter } from 'next/router';
+import { NextComponentType } from 'next';
+import Router from 'next/router';
 
 import { selectors } from '../../redux/authRedux';
 import { actions as authRedux } from '../../redux/authRedux';
@@ -11,57 +11,47 @@ import { authService } from '../../services';
 // import { Router } from '../../routes';
 import { AdminLayoutWrapper } from './AdminLayoutWrapper';
 
-interface PropTypes {
-  router: SingletonRouter;
-}
-
 export const guestOnly = (
   Content: NextComponentType,
   options?: { useAdminLayout: boolean }
 ) => {
-  class GuestWrapper extends React.Component<PropTypes> {
+  class GuestWrapper extends React.Component {
     static async getInitialProps(context) {
-      // const props = {
-      //   req: context.req,
-      //   res: context.res,
-      //   store: context.store,
-      //   isServer: context.isServer,
-      // };
-      // const initialProps = Content.getInitialProps
-      //   ? await Content.getInitialProps(props)
-      //   : {};
-      // if (context.isServer) {
-      //   if (!context.req.cookies.jwt) {
-      //     return initialProps;
-      //   }
-      //   authService.setAccessToken(context.req.cookies.jwt);
-      //   const user = await context.store.dispatch(authRedux.getLoginUser());
-      //   if (user) {
-      //     context.res.redirect('/');
-      //     context.res.end();
-      //   }
-      // } else {
-      //   const user = selectors.getLoginUser(context.store.getState());
-      //   if (user) {
-      //     // Router.pushRoute('/');
-      //   }
-      // }
-      // return { ...initialProps };
-    }
+      const props = {
+        req: context.req,
+        res: context.res,
+        store: context.store,
+        isServer: context.isServer,
+      };
 
-    componentDidMount() {
-      if (this.props.token) {
-        // Router.replaceRoute('/');
+      if (context.isServer) {
+        const jwt = context.req.cookies.jwt;
+
+        if (jwt) {
+          authService.setAccessToken(jwt);
+          const user = await context.store.dispatch(authRedux.getLoginUser());
+
+          if (user) {
+            context.res.redirect('/');
+            context.res.end();
+            return;
+          }
+        }
+      } else {
+        const user = selectors.getLoginUser(context.store.getState());
+
+        if (user) {
+          Router.replace('/');
+          return;
+        }
       }
+
+      return Content.getInitialProps ? Content.getInitialProps(context) : {};
     }
 
     render() {
       if (options && options.useAdminLayout) {
         return this._renderContentInsideAdminLayout();
-      }
-
-      if (this.props.token) {
-        return null;
       }
 
       return <Content {...this.props} />;
