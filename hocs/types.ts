f962433054594d1cc70701db70c5ce0d882a54d6
Tran Dinh Khang <khangtd.09@gmail.com>
2020-01-16 18:35:56 +0700

    feat(boilerplate): typing components

--- hocs/types.ts
+++ hocs/types.ts
@@ -3,8 +3,8 @@ import { NextPageContext } from 'next';
 import { Store } from 'redux';
 
 export interface ExpressReduxNextContext extends NextPageContext {
-  req: express.Request;
-  res: express.Response;
-  store: Store;
-  isServer: boolean;
+  req?: express.Request;
+  res?: express.Response;
+  store?: Store;
+  isServer?: boolean;
 }
