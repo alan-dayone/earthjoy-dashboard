52e0a5dcbcf1265694cb6e401f2198672df59abf
Hao Tang <haotang.io@gmail.com>
2020-03-05 15:19:56 +0700

    fix: add validation for first name and last name

--- view-models/User.ts
+++ view-models/User.ts
@@ -1,6 +1,6 @@
 import * as Yup from 'yup';
 import {AccountStatus} from '../models/User';
-import {constraint as Accountconstraint} from '../models/User';
+import {constraint as AccountConstraint} from '../models/User';
 
 export const AccountStatusText = {
   [AccountStatus.ACTIVE]: 'Active',
@@ -18,19 +18,25 @@ export const userFormValidationSchema = Yup.object().shape({
     .email('Invalid Email.'),
   firstName: Yup.string()
     .required('First name is required')
-    .max(10),
+    .max(
+      AccountConstraint.firstName.MAX_LENGTH,
+      `Last name must be less than ${AccountConstraint.firstName.MAX_LENGTH} characters.`,
+    ),
   lastName: Yup.string()
     .required('Last name is required')
-    .max(20),
+    .max(
+      AccountConstraint.lastName.MAX_LENGTH,
+      `Last name must be less than ${AccountConstraint.lastName.MAX_LENGTH} characters.`,
+    ),
   password: Yup.string()
     .required('Password is required.')
     .min(
-      Accountconstraint.password.MIN_LENGTH,
-      `Password must contain ${Accountconstraint.password.MIN_LENGTH} - ${Accountconstraint.password.MAX_LENGTH} characters.`,
+      AccountConstraint.password.MIN_LENGTH,
+      `Password must contain ${AccountConstraint.password.MIN_LENGTH} - ${AccountConstraint.password.MAX_LENGTH} characters.`,
     )
     .max(
-      Accountconstraint.password.MAX_LENGTH,
-      `Password must contain ${Accountconstraint.password.MIN_LENGTH} - ${Accountconstraint.password.MAX_LENGTH} characters.`,
+      AccountConstraint.password.MAX_LENGTH,
+      `Password must contain ${AccountConstraint.password.MIN_LENGTH} - ${AccountConstraint.password.MAX_LENGTH} characters.`,
     ),
   confirmPassword: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match.'),
 });
