c7e45d40183ca1113214c6c78b60cd88320faa5b
Hao Tang <haotang.io@gmail.com>
2020-04-21 23:06:38 +0700

    fix: sass issue and double render on _app

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /* eslint-disable no-undef, @typescript-eslint/no-var-requires */
 const environmentVariables = require('dotenv').config().parsed;
-const withSass = require('@zeit/next-sass');
 
-const config = withSass();
-
-config.env = environmentVariables;
-
-module.exports = config;
+module.exports = {
+  env: environmentVariables,
+};
