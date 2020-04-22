e978062ba99b8072f9db55445aa9fe7e68e0f89b
thanhle-dayoneteams <thanhle@dayoneteams.com>
2020-04-22 14:46:26 +0700

    refactor: convert to function component

--- pages/admin/reset-new-password.tsx
+++ pages/admin/reset-new-password.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {FC} from 'react';
 import Router from 'next/router';
 import Head from 'next/head';
 import toastr from 'toastr';
@@ -13,111 +13,15 @@ interface ResetNewPasswordForm {
   confirmPassword: string;
 }
 
-class AdminResetNewPasswordPage extends Component {
-  public render(): JSX.Element {
-    return (
-      <div
-        id="admin-login-page"
-        className="align-items-center c-app flex-row pace-done">
-        <Head>
-          <title>Admin - Reset password</title>
-        </Head>
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-md-4">
-              <div className="card-group">
-                <div className="p-4 card">
-                  <div className="card-body">
-                    <Formik
-                      initialValues={{
-                        newPassword: '',
-                        confirmPassword: '',
-                      }}
-                      onSubmit={this._handleResetPassword}
-                      validationSchema={adminResetNewPasswordFormSchema}>
-                      {({
-                        values,
-                        handleChange,
-                        isSubmitting,
-                        handleSubmit,
-                        errors,
-                      }): JSX.Element => (
-                        <form onSubmit={handleSubmit}>
-                          <h1>Reset Password</h1>
-                          <p className="text-muted">
-                            Please enter new password.
-                          </p>
-                          <div className="form-group">
-                            <div className="input-group mb-3">
-                              <div className="input-group-prepend">
-                                <span className="input-group-text">
-                                  <i className="cil-lock-locked" />
-                                </span>
-                              </div>
-                              <input
-                                name="newPassword"
-                                type="password"
-                                placeholder="New Password"
-                                onChange={handleChange}
-                                value={values.newPassword}
-                                className={classnames('form-control', {
-                                  'is-invalid': !!errors.newPassword,
-                                })}
-                              />
-                              {errors.newPassword && (
-                                <div className="invalid-feedback">
-                                  {errors.newPassword}
-                                </div>
-                              )}
-                            </div>
-                          </div>
-                          <div className="form-group">
-                            <div className="input-group mb-3">
-                              <div className="input-group-prepend">
-                                <span className="input-group-text">
-                                  <i className="cil-lock-locked" />
-                                </span>
-                              </div>
-                              <input
-                                name="confirmPassword"
-                                type="password"
-                                placeholder="Confirm Password"
-                                onChange={handleChange}
-                                value={values.confirmPassword}
-                                className={classnames('form-control', {
-                                  'is-invalid': !!errors.confirmPassword,
-                                })}
-                              />
-                              {errors.confirmPassword && (
-                                <div className="invalid-feedback">
-                                  {errors.confirmPassword}
-                                </div>
-                              )}
-                            </div>
-                          </div>
-                          <button
-                            className="btn btn-block btn-primary"
-                            type="submit"
-                            disabled={isSubmitting}>
-                            {isSubmitting && (
-                              <div className="spinner-border spinner-border-sm mr-1" />
-                            )}
-                            Submit
-                          </button>
-                        </form>
-                      )}
-                    </Formik>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+const AdminResetNewPasswordPage: FC = () => {
+  const getErrorMessage = (e): string => {
+    if (e.response?.data?.error?.message === 'invalid_token') {
+      return 'Token is invalid or expired';
+    }
+    return e.message;
+  };
 
-  private _handleResetPassword = async (
+  const handleResetPassword = async (
     values: ResetNewPasswordForm,
     actions: FormikActions<ResetNewPasswordForm>,
   ): Promise<void> => {
@@ -132,19 +36,110 @@ class AdminResetNewPasswordPage extends Component {
       actions.setSubmitting(false);
       await Router.replace('/admin/login');
     } catch (e) {
-      toastr.error(this._getErrorMessage(e));
+      toastr.error(getErrorMessage(e));
       actions.setSubmitting(false);
     }
   };
 
-  private _getErrorMessage(e): string {
-    if (e.response?.data?.error?.message === 'invalid_token') {
-      return 'Token is invalid or expired';
-    }
-
-    return e.message;
-  }
-}
+  return (
+    <div
+      id="admin-login-page"
+      className="align-items-center c-app flex-row pace-done">
+      <Head>
+        <title>Admin - Reset password</title>
+      </Head>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-4">
+            <div className="card-group">
+              <div className="p-4 card">
+                <div className="card-body">
+                  <Formik
+                    initialValues={{
+                      newPassword: '',
+                      confirmPassword: '',
+                    }}
+                    onSubmit={handleResetPassword}
+                    validationSchema={adminResetNewPasswordFormSchema}>
+                    {({
+                      values,
+                      handleChange,
+                      isSubmitting,
+                      handleSubmit,
+                      errors,
+                    }): JSX.Element => (
+                      <form onSubmit={handleSubmit}>
+                        <h1>Reset Password</h1>
+                        <p className="text-muted">Please enter new password.</p>
+                        <div className="form-group">
+                          <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                              <span className="input-group-text">
+                                <i className="cil-lock-locked" />
+                              </span>
+                            </div>
+                            <input
+                              name="newPassword"
+                              type="password"
+                              placeholder="New Password"
+                              onChange={handleChange}
+                              value={values.newPassword}
+                              className={classnames('form-control', {
+                                'is-invalid': !!errors.newPassword,
+                              })}
+                            />
+                            {errors.newPassword && (
+                              <div className="invalid-feedback">
+                                {errors.newPassword}
+                              </div>
+                            )}
+                          </div>
+                        </div>
+                        <div className="form-group">
+                          <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                              <span className="input-group-text">
+                                <i className="cil-lock-locked" />
+                              </span>
+                            </div>
+                            <input
+                              name="confirmPassword"
+                              type="password"
+                              placeholder="Confirm Password"
+                              onChange={handleChange}
+                              value={values.confirmPassword}
+                              className={classnames('form-control', {
+                                'is-invalid': !!errors.confirmPassword,
+                              })}
+                            />
+                            {errors.confirmPassword && (
+                              <div className="invalid-feedback">
+                                {errors.confirmPassword}
+                              </div>
+                            )}
+                          </div>
+                        </div>
+                        <button
+                          className="btn btn-block btn-primary"
+                          type="submit"
+                          disabled={isSubmitting}>
+                          {isSubmitting && (
+                            <div className="spinner-border spinner-border-sm mr-1" />
+                          )}
+                          Submit
+                        </button>
+                      </form>
+                    )}
+                  </Formik>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default guestOnly(AdminResetNewPasswordPage, {
   useAdminLayout: true,
--- pages/admin/reset-password.tsx
+++ pages/admin/reset-password.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {FC, useState} from 'react';
 import Head from 'next/head';
 import toastr from 'toastr';
 import classnames from 'classnames';
@@ -11,105 +11,17 @@ interface ForgotPasswordForm {
   email: string;
 }
 
-interface State {
-  isSubmitted: boolean;
-}
+const AdminResetPasswordPage: FC = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-class AdminResetPasswordPage extends React.Component<{}, State> {
-  public state: State = {
-    isSubmitted: false,
-  };
-
-  public render(): JSX.Element {
-    return (
-      <div
-        id="admin-reset-password-page"
-        className="align-items-center c-app flex-row pace-done">
-        <Head>
-          <title>Admin - Forgot password</title>
-        </Head>
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-md-5">
-              <div className="card-group">
-                <div className="card p-4">
-                  <div className="card-body">
-                    <Formik
-                      initialValues={{email: ''}}
-                      onSubmit={this._handleForgotPassword}
-                      validationSchema={adminResetPasswordFormSchema}>
-                      {(props: FormikProps<{email: string}>): JSX.Element => (
-                        <form onSubmit={props.handleSubmit}>
-                          <h1>Forgot password</h1>
-                          {!this.state.isSubmitted ? (
-                            <div>
-                              <p className="text-muted">
-                                Please enter your email address. We will send
-                                you an email to reset your password.
-                              </p>
-                              <div className="form-group">
-                                <div className="input-group mb-3">
-                                  <div className="input-group-prepend">
-                                    <span className="input-group-text">
-                                      <i className="cil-envelope-closed" />
-                                    </span>
-                                  </div>
-                                  <input
-                                    name="email"
-                                    type="text"
-                                    placeholder="Email"
-                                    onChange={props.handleChange}
-                                    value={props.values.email}
-                                    className={classnames('form-control', {
-                                      'is-invalid': props.errors.email,
-                                    })}
-                                  />
-                                  {props.errors.email && (
-                                    <div className="invalid-feedback">
-                                      {props.errors.email}
-                                    </div>
-                                  )}
-                                </div>
-                              </div>
-                              <button
-                                className="btn btn-block btn-primary"
-                                type="submit"
-                                disabled={props.isSubmitting}>
-                                {props.isSubmitting && (
-                                  <div className="spinner-border spinner-border-sm mr-1" />
-                                )}
-                                Submit
-                              </button>
-                            </div>
-                          ) : (
-                            <div className="mt-3">
-                              <div className="alert alert-success">
-                                We&#39ve just sent you an email to reset your
-                                password.
-                              </div>
-                            </div>
-                          )}
-                        </form>
-                      )}
-                    </Formik>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  public _handleForgotPassword = async (
+  const handleForgotPassword = async (
     values: ForgotPasswordForm,
     actions: FormikActions<ForgotPasswordForm>,
   ): Promise<void> => {
     actions.setSubmitting(true);
     try {
       await authService.sendResetPasswordEmail(values.email);
-      this.setState({isSubmitted: true});
+      setIsSubmitted(true);
       toastr.success('Success');
       actions.setSubmitting(false);
     } catch (e) {
@@ -117,6 +29,86 @@ class AdminResetPasswordPage extends React.Component<{}, State> {
       actions.setSubmitting(false);
     }
   };
-}
+
+  return (
+    <div
+      id="admin-reset-password-page"
+      className="align-items-center c-app flex-row pace-done">
+      <Head>
+        <title>Admin - Forgot password</title>
+      </Head>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-5">
+            <div className="card-group">
+              <div className="card p-4">
+                <div className="card-body">
+                  <Formik
+                    initialValues={{email: ''}}
+                    onSubmit={handleForgotPassword}
+                    validationSchema={adminResetPasswordFormSchema}>
+                    {(props: FormikProps<{email: string}>): JSX.Element => (
+                      <form onSubmit={props.handleSubmit}>
+                        <h1>Forgot password</h1>
+                        {!isSubmitted ? (
+                          <div>
+                            <p className="text-muted">
+                              Please enter your email address. We will send you
+                              an email to reset your password.
+                            </p>
+                            <div className="form-group">
+                              <div className="input-group mb-3">
+                                <div className="input-group-prepend">
+                                  <span className="input-group-text">
+                                    <i className="cil-envelope-closed" />
+                                  </span>
+                                </div>
+                                <input
+                                  name="email"
+                                  type="text"
+                                  placeholder="Email"
+                                  onChange={props.handleChange}
+                                  value={props.values.email}
+                                  className={classnames('form-control', {
+                                    'is-invalid': props.errors.email,
+                                  })}
+                                />
+                                {props.errors.email && (
+                                  <div className="invalid-feedback">
+                                    {props.errors.email}
+                                  </div>
+                                )}
+                              </div>
+                            </div>
+                            <button
+                              className="btn btn-block btn-primary"
+                              type="submit"
+                              disabled={props.isSubmitting}>
+                              {props.isSubmitting && (
+                                <div className="spinner-border spinner-border-sm mr-1" />
+                              )}
+                              Submit
+                            </button>
+                          </div>
+                        ) : (
+                          <div className="mt-3">
+                            <div className="alert alert-success">
+                              We&#39ve just sent you an email to reset your
+                              password.
+                            </div>
+                          </div>
+                        )}
+                      </form>
+                    )}
+                  </Formik>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default guestOnly(AdminResetPasswordPage, {useAdminLayout: true});
