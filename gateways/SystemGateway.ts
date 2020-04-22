c101d7c2f9fab56771cbbbc7f72ad532cbfd1289
Vy Dang <vydangnguyen71@gmail.com>
2020-04-22 18:06:32 +0700

    feat: change function component

--- gateways/SystemGateway.ts
+++ gateways/SystemGateway.ts
@@ -49,6 +49,8 @@ export class SystemGateway {
   public async initSystem(body: {
     password: string;
     admin: {
+      firstName: string;
+      lastName: string;
       email: string;
       password: string;
     };
